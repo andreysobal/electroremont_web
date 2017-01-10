b0d9b1167ab2d93b110858a7e729c6db983b0c85
andreysobal <say6287@gmail.com>
2017-01-10 22:21:27 +0300

    Added slider in top of page

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
- $('.navMenu').click(function() {
-	var menu = $(".navItem");
-	var nav = $(this);
-	if (menu.is(':hidden')) {
-		$(menu).each(function() {
-			$(this).removeClass('hidden-xs').css("border-bottom", "0.5px dotted black");
-			console.log("+++");
-		});//end each
-		nav.css("border-bottom", "0.5px solid black");
-		$("#navigation").css("border-bottom", "none");
-	} else {
-		$(menu).each(function() {
-			$(this).addClass('hidden-xs').css("border-bottom", "none");
-			console.log("---");
-		});//end each
-		nav.css("border-bottom", "none");
-		$("#navigation").css("border-bottom", "0.5px solid black");
-	};
- }); // окончание click
+	$('.autoplay').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 5000,
+	});
+	$('.navMenu').click(function() {
+		var menu = $(".navItem");
+		var nav = $(this);
+		if (menu.is(':hidden')) {
+			$(menu).each(function() {
+				$(this).removeClass('hidden-xs').css("border-bottom", "0.5px dotted black");
+				console.log("+++");
+			});//end each
+			nav.css("border-bottom", "0.5px solid black");
+			$("#navigation").css("border-bottom", "none");
+		} else {
+			$(menu).each(function() {
+				$(this).addClass('hidden-xs').css("border-bottom", "none");
+				console.log("---");
+			});//end each
+			nav.css("border-bottom", "none");
+			$("#navigation").css("border-bottom", "0.5px solid black");
+		};//end if
+	}); // окончание click
 }); // окончание ready
