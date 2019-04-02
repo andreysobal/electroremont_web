4d9fe839af9ea3e67102804e1c9684441815419f
andreysobal <say6287@gmail.com>
2019-04-02 22:38:38 +0300

    Changed text in callback email from web-site

--- mail.php
+++ mail.php
@@ -7,5 +7,5 @@ $name = trim($_GET["name"]);
 $phone = trim($_GET["phone"]);
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone";
+$message = "Заявка на звонок с вэб-сайта от клиента по имени $name , номер телефона $phone";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
